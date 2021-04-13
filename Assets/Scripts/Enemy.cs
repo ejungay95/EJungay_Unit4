2fee252e86361770941a9801daac598c66de376b
ejungay95 <elijah.jungay@gmail.com>
2021-04-12 19:33:19 -0700

    Finished Unit 4: Lesson B

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  Rigidbody enemyRb;
+  GameObject player;
+
+  public float speed = 4.0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    enemyRb = GetComponent<Rigidbody>();
+    player = GameObject.Find("Player");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Vector3 seekDirection = (player.transform.position - transform.position).normalized;
+    enemyRb.AddForce(seekDirection * speed * Time.deltaTime, ForceMode.Impulse);
+  }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+  private float spawnRange = 8.5f;
+  public GameObject enemyPrefab;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+
+  Vector3 GenerateSpawnPosition()
+  {
+    float xPos = Random.Range(-spawnRange, spawnRange);
+    float zPos = Random.Range(-spawnRange, spawnRange);
+    Vector3 randSpawnPos = new Vector3(xPos, enemyPrefab.transform.position.y, zPos);
+    return randSpawnPos;
+  }
+}
